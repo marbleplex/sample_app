2769ac0f8d32c3cf3ff62e2879e1975c7b8d16b0
aki <marbleplex@gmail.com>
2013-04-17 20:50:34 +0900

    add contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
 	describe "Home pages" do
 		it "should have the h1 'Sample App'" do	
 			visit '/static_pages/home'
@@ -9,7 +11,7 @@ describe "Static pages" do
 		end
 		it "should have the title 'Home'" do
 			visit '/static_pages/home'
-			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+			page.should have_selector('title', :text => "#{base_title} | Home")
 		end
 	end
 	
@@ -20,7 +22,7 @@ describe "Static pages" do
 		end
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
-			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+			page.should have_selector('title', :text => "#{base_title} | Help")
 		end
 	end
 
@@ -31,14 +33,18 @@ describe "Static pages" do
 		end
 		it "should have the title 'About Us'" do
 			visit '/static_pages/about'
-			page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | About Us')
+			page.should have_selector('title', :text => "About Us")
 		end
 	end
-# describe "GET /static_pages" do
-#   it "works! (now write some real specs)" do
-#     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-#     get static_pages_index_path
-#     response.status.should be(200)
-#   end
-# end
+
+	describe "Contact page" do
+		it "should have the h1 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact')
+		end
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | Contact')
+		end
+	end
 end
